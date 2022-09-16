f4fc3fbeed4d05e68efa858b8a690328027d5a73
Nick Pavlovic <nickpavlovic@icloud.com>
2022-09-16 17:49:00 -0400

    variable and lookup dictionary assigned

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,35 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+let alphaLowerCase= "abcdefghijklmnopqrstuvwxyz";
+let alphaUpperCase = alphaLowerCase.toUpperCase();
+let numeric = "0123456789";
+let allowedSymbols = "!@#$%^&*+?";
+let LowerCaseLetters = alphaLowerCase.split("");
+let UpperCaseLetters = alphaUpperCase.split("");
+let symbols = allowedSymbols.split("");
+let numbers = numeric.split("");
+
+let min_password_length = 8;
+let max_password_length = 128;
+let [incUpperCase, incLowerCase, incNumeric, incSpecialChar] = [true,true,true,true];
+let userPasswordLength = 0;
+
+
+console.log(LowerCaseLetters);
+console.log(UpperCaseLetters);
+console.log(symbols);
+console.log(numbers);
+
+function generatePassword() {
+
+}
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
